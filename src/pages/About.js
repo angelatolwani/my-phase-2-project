cb5dbe19b12c4e9c832596636b2b32416f67831a
Angela Tolwani <angela@tolwani.com>
2024-05-30 01:34:47 -0400

    populate home and about pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,63 @@
+import { Box, Container } from "@mui/material";
+
 function About () {
     return (
         <>
-            <h1>About</h1>
+            <Container maxWidth="sm">
+                <Box>
+                    <h1 align="center">Virtual Consult Guidelines</h1>
+                    <p>Our innovative approach offers the convenience of expert 
+                        veterinary input potentially avoiding a physical visit to 
+                        the veterinary hospital.</p>
+                    <p>Veterinarians are able to establish a veterinarian-client-patient 
+                        relationship (VCPR) through remote methods.</p>
+                    <h4>Benefits of Joining the Network</h4>
+                    <ol>
+                        <li>
+                            Flexible Scheduling
+                            <ul>
+                                <li>Set your own hours to balance professional and personal life with ease.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            Own Your Business
+                            <ul>
+                                <li>Run a turn-key veterinary consulting business.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            Earn More
+                            <ul>
+                                <li>Grow your earnings beyond typical associate veterinarian salaries.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            Expand Your Client Base
+                            <ul>
+                                <li>Connect with pet owners at their convenience, anytime, anywhere.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            AI-Powered Efficiency
+                            <ul>
+                                <li>Focus on caring for pets while we handle documentation.</li>
+                            </ul>
+                        </li>
+                    </ol>
+                    <h4>Key Guidelines</h4>
+                    <ul>
+                        <li>A pet parent authorizes the veterinarian to assume 
+                            responsibility for medical care.</li>
+                        <li>The veterinarian is responsible for making medical 
+                            judgements and developing a treatment plan.</li>
+                        <li>The veterinarian must have sufficient info of the animal patient.</li>
+                        <li>The pet parent agrees to have a virtual consult and is aware of 
+                            the potential limitations of virtual consults.  </li>
+                        <li>The VCPR relationship must be reestablished every 6 months to allow the 
+                            veterinarian to diagnose, treat and prescribe medications. </li>
+                    </ul>
+                </Box>
+            </Container>            
         </>
     )
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,51 @@
+import { Grid, Card, CardContent, Typography, CardMedia, Paper, Box } from "@mui/material";
+
 function Home () {
+    const imgUrl = "https://t3.ftcdn.net/jpg/06/38/69/12/240_F_638691287_sSY3HcDjVZ1tKNL971kT70T1YnrC3mvm.jpg"
     return (
         <>
-            <h1>Vet Telehealth</h1>
+            <Paper
+                sx={{
+                    position: 'relative',
+                    backgroundColor: 'grey.800',
+                    color: '#fff',
+                    mb: 4,
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
+                    backgroundImage: `url(${imgUrl})`,
+                }}
+            >
+                {<img style={{ display: 'none' }} src={imgUrl} alt="animals" />}
+                <Box
+                    sx={{
+                    position: 'absolute',
+                    top: 0,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    backgroundColor: 'rgba(0,0,0,.3)',
+                    }}
+                />
+                <Grid container>
+                    <Grid item md={12}>
+                        <Box
+                            sx={{
+                                position: 'relative',
+                                p: { xs: 24 },
+                                pr: { md: 0 },
+                            }}
+                        >
+                            <Typography component="h2" variant="h3" color="inherit" gutterBottom>
+                                Virtual Vet Care
+                            </Typography>
+                            <Typography variant="h5" color="inherit" paragraph>
+                                Innovating for Healthier and Happier Pets
+                            </Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Paper>
         </>
     )
 }
